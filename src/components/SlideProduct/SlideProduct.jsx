6df6dda32774d0edc7d060fa75a0d2ae127b1eb1
SondosMashahreah <sundos.mashahreh@students.alquds.edu>
2026-09-13 12:18:21 +0300

    Revert "Fix mobile header and product slider layout"
    This reverts commit feb9afc476f15980e9b5990c646e8385185e5f55.

--- src/components/SlideProduct/SlideProduct.jsx
+++ src/components/SlideProduct/SlideProduct.jsx
@@ -39,8 +39,8 @@ function SlideProduct({
   spaceBetween={15}
   breakpoints={{
     0: {
-      slidesPerView: 2,
-      spaceBetween: 8,
+      slidesPerView: 1.2,
+      spaceBetween: 10,
     },
     480: {
       slidesPerView: 2,
